3f0c4e7772ea283af6e0611c00e8bd0b265c9d2a
Alexander <aerzin-ic@yandex.ru>
2026-05-19 17:56:25 +0300

    добавлена верстка игры

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "@/shared/lib/zod-init"; // Русская локализация ошибок zod — до всех остальных импортов
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,11 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col dark">{children}</body>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased flex flex-col grow min-h-screen dark`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
